3ae43fe33fac06006450d8ab1aa9e114e1e575d3
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2023-10-19 16:57:03 +0530

    binary to decimal

---
+++ display.js
@@ -0,0 +1,19 @@
+binaryToDecimal(90);
+
+function binaryToDecimal(n) 
+{ 
+    let num = n; 
+    let dec_value = 0; 
+  
+    // Initializing base value to 1, i.e 2^0 
+    let base = 1; 
+  
+    let temp = num; 
+    while (temp) { 
+        let last_digit = temp % 10; 
+        temp = Math.floor(temp / 10); 
+  
+        dec_value += last_digit * base; 
+  
+        base = base * 2; 
+    } 
